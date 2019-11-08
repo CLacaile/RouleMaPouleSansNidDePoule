45037eed2b93f2889f6beffb3e199797bf27e91c
CLacaile <clementlacaile@gmail.com>
2019-11-08 09:57:36 +0100

    changed database setting

--- RouleMaPoule/RouleMaPoule/settings.py
+++ RouleMaPoule/RouleMaPoule/settings.py
@@ -74,11 +74,11 @@ WSGI_APPLICATION = 'RouleMaPoule.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+     'default': {
+         'ENGINE': 'djongo',
+         'NAME': 'RouleMaPoule_DB',
+     }
+ }
 
 
 # Password validation
