372e59bf15a22339c79abc06003a733fd0fda0ea
Nathan Sarniguet <nathsarniguet@msn.com>
2019-11-15 11:59:38 +0100

    Update Models + initial_data

--- RouleMaPoule/input/models/acceleration.py
+++ RouleMaPoule/input/models/acceleration.py
@@ -4,7 +4,8 @@ from .waypoint import Waypoint
 
 class Acceleration(models.Model):
     id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField(auto_now_add=False)
     accelx = models.IntegerField()
     accely = models.IntegerField()
     accelz = models.IntegerField()
-    waypoint = models.ForeignKey(Waypoint, on_delete=models.CASCADE)
+    waypoint = models.ForeignKey(Waypoint, on_delete=models.CASCADE, related_name="accelerations")
--- RouleMaPoule/input/models/waypoint.py
+++ RouleMaPoule/input/models/waypoint.py
@@ -4,7 +4,6 @@ from .path import Path
 
 class Waypoint(models.Model):
     id = models.AutoField(primary_key=True)
-    timestamp = models.DateTimeField(auto_now_add=False)
     longitude = models.FloatField()
     latitude = models.FloatField()
-    path = models.ForeignKey(Path, on_delete=models.CASCADE)
+    path = models.ForeignKey(Path, on_delete=models.CASCADE, related_name="waypoints")
