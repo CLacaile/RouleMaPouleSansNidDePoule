d04bd767b7c75333beefa944a6c9ab3f6a433ef1
CLacaile <clementlacaile@gmail.com>
2019-11-22 12:21:42 +0100

    implemented api access functions to objects Path, Waypoint, Acceleration

--- RouleMaPoule/input/views.py
+++ RouleMaPoule/input/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Path, Waypoint, Acceleration
+from input.serializers import PathSerializer, WaypointSerializer, AccelerationSerializer
 
-# Create your views here.
+class PathViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Path to be viewed or edited    
+    """
+    queryset = Path.objects.all()
+    serializer_class = PathSerializer
+
+class WaypointViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Waypoint to be viewed or edited    
+    """
+    queryset = Waypoint.objects.all()
+    serializer_class = WaypointSerializer
+
+class AccelerationViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Acceleration to be viewed or edited    
+    """
+    queryset = Acceleration.objects.all()
+    serializer_class = AccelerationSerializer
