795d89ccb9d47e76474fa2372511ad3e93ee6abe
CLacaile <clementlacaile@gmail.com>
2019-11-22 12:22:07 +0100

    initial commit

---
+++ RouleMaPoule/input/serializers.py
@@ -0,0 +1,20 @@
+from .models import Path, Waypoint, Acceleration
+from rest_framework import serializers
+
+
+class PathSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Path
+        fields = ['id', 'id_sensor']
+
+
+class WaypointSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Waypoint
+        fields = ['id', 'longitude', 'latitude', 'path']
+
+
+class AccelerationSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Acceleration
+        fields = ['id', 'timestamp', 'accelx', 'accely', 'accelz', 'waypoint']
