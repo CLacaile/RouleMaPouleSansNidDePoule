950f0f26cdb4bb2953f2241b408f37497933f748
guillem <guillem.orelle@gmail.com>
2019-11-25 17:31:20 +0100

    WIP output

--- RouleMaPoule/output/models/trust_rate.py
+++ RouleMaPoule/output/models/trust_rate.py
@@ -1,7 +1,9 @@
 from django.db import models
+from input.models import *
 
 
 class TrustRate(models.Model):
     id = models.AutoField(primary_key=True)
     timestamp = models.DateTimeField(auto_now_add=False)
     rate = models.FloatField()
+    trust_waypoint = models.ForeignKey(Waypoint, on_delete=models.CASCADE, related_name="trustrate")
--- RouleMaPoule/output/views.py
+++ RouleMaPoule/output/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import *
+from rest_framework import viewsets
+from output.serializers import *
+
 
 # Create your views here.
+class TrustRateViewSet(viewsets.ModelViewSet):
+    queryset = TrustRate.objects.all()
+    serializer_class = TrustRate
+
+
+class RoadGradeViewSet(viewsets.ModelViewSet):
+    queryset = RoadGrade.objects.all()
+    serializer_class = RoadGrade
