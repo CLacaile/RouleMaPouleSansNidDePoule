fadf25467f5095bba902dc9b033ad8c86a7d6979
Nathan Sarniguet <nathsarniguet@msn.com>
2019-11-25 11:20:38 +0100

    Create csv_upload.py

--- RouleMaPoule/input/views.py
+++ RouleMaPoule/input/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Path, Waypoint, Acceleration
+from rest_framework.exceptions import ParseError
 from input.serializers import PathSerializer, WaypointSerializer, AccelerationSerializer
 from rest_framework import viewsets
+from rest_framework import views
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -27,12 +29,17 @@ class AccelerationViewSet(viewsets.ModelViewSet):
     queryset = Acceleration.objects.all()
     serializer_class = AccelerationSerializer
 
+class CSVFileUploadParser(FileUploadParser):
+    media_type = 'text/csv'
+
 class FileUploadView(views.APIView):
-    parser_classes = [FileUploadParser]
+    parser_classes = [CSVFileUploadParser]
 
     def put(self, request, filename, format=None):
-        file_obj = request.data['file']
-        # ...
-        # do some stuff with uploaded file
-        # ...
+        if 'file' not in request.data:
+            raise ParseError("Empty content")
+        csv_file = request.data['file']
+
+
+
         return Response(status=204)
