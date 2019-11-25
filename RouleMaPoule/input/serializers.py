7fe53cac9fe2f0be489a92a87c4ec378712dd7ef
CLacaile <clementlacaile@gmail.com>
2019-11-25 11:06:21 +0100

    added FileUploadView class to upload CSV

--- RouleMaPoule/input/serializers.py
+++ RouleMaPoule/input/serializers.py
@@ -1,6 +1,8 @@
 from .models import Path, Waypoint, Acceleration
 from rest_framework import serializers
-
+from rest_framework.parsers import FileUploadParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 class PathSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -18,3 +20,14 @@ class AccelerationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Acceleration
         fields = ['id', 'timestamp', 'accelx', 'accely', 'accelz', 'waypoint']
+
+
+class FileUploadView(views.APIView):
+    parser_classes = [FileUploadParser]
+
+    def put(self, request, filename, format=None):
+        file_obj = request.data['file']
+        # ...
+        # do some stuff with uploaded file
+        # ...
+        return Response(status=204)
