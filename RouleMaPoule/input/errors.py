95a5056bcb93b54afc9d5696a3f17516709a7eab
CLacaile <clementlacaile@gmail.com>
2019-11-26 17:04:30 +0100

    cleaned csv_upload.py file and moved Errors and Exceptions to to new seperate files : exceptions.py and errors.py

---
+++ RouleMaPoule/input/errors.py
@@ -0,0 +1,12 @@
+from RouleMaPoule.input.exceptions import Error
+
+class WrongNumberOfColumns(Error):
+    pass
+
+
+class WrongAccelerationValue(Error):
+    pass
+
+
+class WrongGPSData(Error):
+    pass
--- RouleMaPoule/input/processing/csv_upload.py
+++ RouleMaPoule/input/processing/csv_upload.py
@@ -1,9 +1,11 @@
 import csv
 import logging
-logger = logging.getLogger(__name__)
-
+import traceback
+from datetime import datetime
 from input.models import Path, Waypoint, Acceleration
+from RouleMaPoule.input.errors import WrongNumberOfColumns, WrongGPSData, WrongAccelerationValue
 
+# CONSTANTS
 DATE = 0
 ID_SENSOR = 1
 LATITUDE = 2
@@ -11,30 +13,30 @@ LONGITUDE = 3
 ACCELX = 4
 ACCELY = 5
 ACCELZ = 6
-
 NOT_DEFINED = -666
 
-import traceback
-from datetime import datetime
+# Logger
+logger = logging.getLogger(__name__)
 
 
-class Error(Exception):
-    pass
+def check_csv(data):
+    """
+    Function that checks whether a row of CSV is conform or not. It checks :
+    - If the CSV has more or less than 7 columns
+    - Parses the date to a datetime
+    - Casts the sensor ID to an int
+    - Checks whether the latitude given is between 90 and -90
+    - Checks whether the longitude is between 180 and -180
+    - Checks whether accelx, accely and accelz is between 0 and 65535
 
-
-class WrongNumberOfColumns(Error):
-    pass
-
-
-class WrongAccelerationValue(Error):
-    pass
-
-
-class WrongGPSData(Error):
-    pass
-
-
-def check_csv(data ):
+    Args:
+        - data a row of a CSV file
+    
+    Raises:
+        - WrongNumberOfColumns
+        - WrongGPSData
+        - WrongAccelerationValue
+    """
     if len(data) < 7 or len(data) > 7:
         raise WrongNumberOfColumns
 
@@ -71,6 +73,13 @@ def check_csv(data ):
                 raise WrongAccelerationValue
 
 def csv_upload(csv_file):
+    """ 
+    This function uploades parses a given CSV file into objects and 
+    store them in the db, after having checked the file using check_csv.
+    
+    Args:
+        - csv_file a CSV file to insert into the db
+    """
         #Check if file is CSV
         #Check if file is empty
 
