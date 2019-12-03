21de0457c2a3e5b91b0c8d4c21a9918dc57924f0
Nathan Sarniguet <nathsarniguet@msn.com>
2019-12-03 17:53:38 +0100

    Tests csv_upload

--- RouleMaPoule/input/processing/csv_upload.py
+++ RouleMaPoule/input/processing/csv_upload.py
@@ -29,6 +29,7 @@ def check_csv(fields):
     - Checks whether the longitude is between 180 and -180
     - Checks whether accelx, accely and accelz is between 0 and 65535
     """
+    nbFields = len(fields)
     if len(fields) < 7 or len(fields) > 7:
         raise WrongNumberOfColumns
 
@@ -64,41 +65,6 @@ def check_csv(fields):
             if int(fields[i]) > 65535 or int(fields[i]) < 0:
                 raise WrongAccelerationValue
 
-def check_csv(fields):
-    if len(fields) < 7 or len(fields) > 7:
-        raise WrongNumberOfColumns
-
-    for i in range(len(fields)):
-        if i == DATE:
-            datetime.strptime(fields[i], '%Y-%m-%d %H:%M:%S')
-
-        if i == ID_SENSOR:
-            int(fields[i])
-
-        if i == LATITUDE:
-            float(fields[i])
-            if float(fields[i]) > 90 or float(fields[i]) < -90:
-                raise WrongGPSData
-
-        if i == LONGITUDE:
-            float(fields[i])
-            if float(fields[i]) > 180 or float(fields[i]) < -180:
-                raise WrongGPSData
-
-        if i == ACCELX:
-            int(fields[i])
-            if int(fields[i]) > 65535 or int(fields[i]) < 0:
-                raise WrongAccelerationValue
-
-        if i == ACCELY:
-            int(fields[i])
-            if int(fields[i]) > 65535 or int(fields[i]) < 0:
-                raise WrongAccelerationValue
-
-        if i == ACCELZ:
-            int(fields[i])
-            if int(fields[i]) > 65535 or int(fields[i]) < 0:
-                raise WrongAccelerationValue
 
 def csv_upload(csv_file):
     """
@@ -109,49 +75,52 @@ def csv_upload(csv_file):
             - csv_file a CSV file to insert into the db
     """
 
-    #init Path
-    id_sensor = NOT_DEFINED
-    last_waypoint = Waypoint()
-    waypoint = Waypoint()
-    path = Path()
-    path.save()
-
-    index = 1
-
     lines = csv_file.split("\n")
-    for line in lines:
-        if(index>1):
-            fields = line.split(",")
-            try:
-                check_csv(fields)
-                if (id_sensor == NOT_DEFINED):
-                    try:
-                        path.id_sensor = int(fields[ID_SENSOR])
-                    except:
-                        logger.warning("CSV import : row " + str(index) + " INVALID id_sensor")
-                if (fields[LATITUDE] != last_waypoint.latitude or fields[LONGITUDE] != last_waypoint.latitude):
-                    waypoint = Waypoint(latitude=float(fields[LATITUDE]), longitude=float(fields[LONGITUDE]))
+    if(len(lines) > 1):
+
+        # init Path
+        id_sensor = NOT_DEFINED
+        last_waypoint = Waypoint()
+        waypoint = Waypoint()
+        path = Path()
+        path.save()
+
+        index = 1
+        for line in lines:
+            if(index>1):
+                fields = line.split(",")
+                try:
+                    check_csv(fields)
+                    if (id_sensor == NOT_DEFINED):
+                        try:
+                            path.id_sensor = int(fields[ID_SENSOR])
+                        except:
+                            logger.warning("CSV import : row " + str(index) + " INVALID id_sensor")
+                    if (fields[LATITUDE] != last_waypoint.latitude or fields[LONGITUDE] != last_waypoint.latitude):
+                        waypoint = Waypoint(latitude=float(fields[LATITUDE]), longitude=float(fields[LONGITUDE]))
+                        waypoint.save()
+                        path.waypoints.add(waypoint)
+                    acceleration = Acceleration(timestamp=fields[DATE], accelx=fields[ACCELX], accely=fields[ACCELY],
+                                                accelz=fields[ACCELZ])
+                    acceleration.save()
+                    waypoint.accelerations.add(acceleration)
                     waypoint.save()
-                    path.waypoints.add(waypoint)
-                acceleration = Acceleration(timestamp=fields[DATE], accelx=fields[ACCELX], accely=fields[ACCELY],
-                                            accelz=fields[ACCELZ])
-                acceleration.save()
-                waypoint.accelerations.add(acceleration)
-                waypoint.save()
-            except WrongNumberOfColumns:
-                logger.warning("CSV import : row " + str(index) + " Wrong number of columns")
+                except WrongNumberOfColumns:
+                    logger.warning("CSV import : row " + str(index) + " Wrong number of columns")
 
-            except WrongGPSData:
-                logger.warning("CSV import : row " + str(index) + " Wrong GPS data")
+                except WrongGPSData:
+                    logger.warning("CSV import : row " + str(index) + " Wrong GPS data")
 
-            except WrongAccelerationValue:
-                logger.warning("CSV import : row " + str(index) + " The acceleration number is out of bounds")
+                except WrongAccelerationValue:
+                    logger.warning("CSV import : row " + str(index) + " The acceleration number is out of bounds")
 
-            except ValueError:
-                tb = traceback.format_exc()
-                logger.warning("CSV import : row " + str(index) + " " + tb.title())
+                except ValueError:
+                    tb = traceback.format_exc()
+                    logger.warning("CSV import : row " + str(index) + " " + tb.title())
 
-            except Exception as e:
-                logger.warning("CSV import : row " + str(index) + " " + str(e))
-        index += 1
-    path.save()
+                except Exception as e:
+                    logger.warning("CSV import : row " + str(index) + " " + str(e))
+            index += 1
+        path.save()
+        return path.id
+    return None
--- RouleMaPoule/input/tests.py
+++ RouleMaPoule/input/tests.py
@@ -1,17 +1,146 @@
 
 from django.test import TestCase
-from random import seed
+from random import seed, random, uniform
 from random import randint
 from datetime import datetime as dt
 
 from .models import Path, Waypoint, Acceleration
-from .processing import csv_upload, calculation_logic
+from .processing import csv_upload as CSV, calculation_logic
+from input.errors import WrongNumberOfColumns, WrongGPSData, WrongAccelerationValue
+
 
 class CSV_import_Tests(TestCase):
 
-    def test_import_csv(self):
-        csv_file= open('test.csv', 'r+')
-        csv_upload.csv_upload(csv_file.read())
+
+    def setUp(self):
+        self.timestamp = "2019-03-03 22:10:10"
+        self.sensor_id = str(randint(0, 100))
+        self.latitude = str(round(uniform(-90, 90), 4))
+        self.longitude = str(round(uniform(-180, 180), 4))
+        self.accelx = str(randint(0, 65535))
+        self.accely = str(randint(0, 65535))
+        self.accelz = str(randint(0, 65535))
+
+    def test_check_csv_ok(self):
+        try:
+            csv_line = [self.timestamp, self.sensor_id, self.latitude, self.longitude, self.accelx, self.accely,
+                        self.accelz]
+            CSV.check_csv(csv_line)
+            self.assert_(True)
+        except Exception as e:
+            self.assert_(False)
+
+    def test_check_csv_invalid_date_format(self):
+        try:
+            csv_line = ["2012-28-98", self.sensor_id, self.latitude, self.longitude, self.accelx, self.accely,
+                        self.accelz]
+            CSV.check_csv(csv_line)
+            self.assert_(False)
+        except ValueError as e:
+            self.assert_(True)
+
+    def test_check_csv_invalid_wrong_number_of_column(self):
+        try:
+            csv_line = [self.timestamp, self.sensor_id, self.latitude, self.longitude, self.accelx, self.accely]
+            CSV.check_csv(csv_line)
+            self.assert_(False)
+        except WrongNumberOfColumns as e:
+            self.assert_(True)
+        try:
+            csv_line = [self.timestamp, self.sensor_id, self.latitude, self.longitude, self.accelx, self.accely,
+                        self.accelz, "oneMoreColumn"]
+            CSV.check_csv(csv_line)
+            self.assert_(False)
+        except WrongNumberOfColumns as e:
+            self.assert_(True)
+
+    def test_check_csv_invalid_longitude(self):
+        try:
+            csv_line = [self.timestamp, self.sensor_id, "1000000", self.longitude, self.accelx, self.accely,
+                        self.accelz]
+            CSV.check_csv(csv_line)
+            self.assert_(False)
+        except WrongGPSData as e:
+            self.assert_(True)
+        try:
+            csv_line = [self.timestamp, self.sensor_id, "-1000000", self.longitude, self.accelx, self.accely,
+                        self.accelz]
+            CSV.check_csv(csv_line)
+            self.assert_(False)
+        except WrongGPSData as e:
+            self.assert_(True)
+
+    def test_check_csv_invalid_latitude(self):
+        try:
+            csv_line = [self.timestamp, self.sensor_id, self.latitude, "1000000", self.accelx, self.accely,
+                        self.accelz]
+            CSV.check_csv(csv_line)
+            self.assert_(False)
+        except WrongGPSData as e:
+            self.assert_(True)
+        try:
+            csv_line = [self.timestamp, self.sensor_id, self.latitude, "-1000000", self.accelx, self.accely,
+                        self.accelz]
+            CSV.check_csv(csv_line)
+            self.assert_(False)
+        except WrongGPSData as e:
+            self.assert_(True)
+
+    def test_check_csv_invalid_acceleration_value(self):
+        try:
+            csv_line = [self.timestamp, self.sensor_id, self.latitude, self.longitude, "100000", self.accely,
+                        self.accelz]
+            CSV.check_csv(csv_line)
+            self.assert_(False)
+        except WrongAccelerationValue as e:
+            self.assert_(True)
+        try:
+            csv_line = [self.timestamp, self.sensor_id, self.latitude, self.latitude, "-1000", self.accely,
+                        self.accelz]
+            CSV.check_csv(csv_line)
+            self.assert_(False)
+        except WrongAccelerationValue as e:
+            self.assert_(True)
+
+    def test_csv_upload_ok(self):
+        pathCount = Path.objects.count()
+        waypointCount = Waypoint.objects.count()
+        accelerationCount = Acceleration.objects.count()
+
+        #write in the csv file
+        csv_file = open('test.csv', 'w+')
+        csv_file.write("timestamp,id_sensor,longitude,latitude,AccelX,AccelY,AccelZ\n")
+        csv_line = self.timestamp+","+self.sensor_id+","+self.latitude+","+self.longitude+","+self.accelx+","+self.accely+","+self.accelz
+        csv_file.write(csv_line)
+        csv_file.close()
+
+        #parse and insert
+        csv_file = open('test.csv', 'r+')
+        pathId = CSV.csv_upload(csv_file.read())
+        csv_file.close()
+
+        self.assertIs(pathCount+1, Path.objects.count())
+        self.assertIs(waypointCount+1, Waypoint.objects.count())
+        self.assertIs(accelerationCount+1, Acceleration.objects.count())
+
+        path = Path.objects.get(id=pathId)
+        self.assertIs(path.id_sensor, int(self.sensor_id))
+        self.assertIs(path.waypoints.count(), 1)
+
+        waypoint = path.waypoints.first()
+        self.assertAlmostEqual(waypoint.latitude, float(self.latitude))
+        self.assertAlmostEqual(waypoint.longitude, float(self.longitude))
+        self.assertIs(waypoint.accelerations.count(), 1)
+
+        acceleration = waypoint.accelerations.first()
+        self.assertAlmostEqual(acceleration.accelx, int(self.accelx))
+        self.assertAlmostEqual(acceleration.accely, int(self.accely))
+        self.assertAlmostEqual(acceleration.accelz, int(self.accelz))
+
+
+    def test_csv_upload_endpoint(self):
+        csv_file = open('test.csv', 'r+')
+        #csv.csv_upload(csv_file.read())
         self.assertIs(False, False)
 
 class calc_logic_tests(TestCase):
