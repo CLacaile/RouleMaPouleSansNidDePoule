f0fbd6ee92447f2b87f679b6005e9665551ac0cf
guillem <guillem.orelle@gmail.com>
2019-12-03 17:41:45 +0100

    fix virgule

--- RouleMaPoule/RouleMaPoule/urls.py
+++ RouleMaPoule/RouleMaPoule/urls.py
@@ -38,7 +38,7 @@ router.register(r'api/v1.0/output/trustrate', TrustRateViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path(r'api/v1.0/input/upload', FileUploadView.as_view())
+    path(r'api/v1.0/input/upload', FileUploadView.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh')
